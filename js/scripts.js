4f20b267ab97da6685ba2d13b485253c37314519
Michaela Davis <michaela.delight@gmail.com>
2017-01-12 12:16:58 -0800

    add info/adult pics to creature wells

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+  $(".well").click(function(event) {
+    $(this).children().toggle();
+  });
+  // $(".well").click(function(){
+  //   $(this).children().toggle();
+  // });
+
 
 
   event.preventDefault();
